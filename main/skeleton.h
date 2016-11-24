96d8827d1ebda303435236d647b265807ed094a9
Khang Nguyen <khang.neon.1997@gmail.com>
2016-11-24 20:55:01 +0700

    [UPDATE] Init project skeleton

---
+++ main/skeleton.h
@@ -0,0 +1,21 @@
+#ifndef SKELETION_H
+#define SKELETION_H
+
+void e1();
+void e2();
+void e3();
+void e4();
+void e5();
+void e6();
+void e7();
+void e8();
+void e9();
+void e10();
+void e11();
+void e12();
+void e13();
+void e14();
+void e15();
+void e16();
+
+#endif
