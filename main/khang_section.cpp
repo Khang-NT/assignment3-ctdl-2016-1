980e01cef8d9c93e2da12d9d829727d6aa81f765
Khang Nguyen <khang.neon.1997@gmail.com>
2016-11-25 01:18:30 +0700

    [UPDATE] khang_section: done E5

--- main/khang_section.cpp
+++ main/khang_section.cpp
@@ -1,1 +1,40 @@
 #include "skeleton.h"
+#include "../InputReader.h"
+#include "../Graph.h"
+
+
+void Graph::Print() {
+  Vertex* vertex = this->gHead;
+  Edge* edge;
+  while (vertex) {
+    edge = vertex->firstEdge;
+    std::cout << vertex->data;
+    while (edge) {
+      std::cout << " -> " << edge->destination->data;
+      edge = edge->nextEdge;
+    }
+    std::cout << " -> NULL\n|\n";
+    vertex = vertex->nextVertex;
+  }
+  std::cout << "NULL\n";
+}
+
+void e5() {
+  int* vertexDataArr;
+  int vertexCount;
+  int** edgeDataArr;
+  int edgeCount;
+  ReadArrayInputOfGraph("input/E5.txt", vertexDataArr, vertexCount, edgeDataArr, edgeCount);
+  Graph graph;
+  for (size_t i = 0; i < vertexCount; i++) {
+    graph.InsertVertex(vertexDataArr[i]);
+  }
+  for (size_t i = 0; i < edgeCount; i++) {
+    graph.InsertEdge(edgeDataArr[i][0], edgeDataArr[i][1]);
+    delete[] edgeDataArr[i];
+  }
+  graph.Print();
+  // collect garbage
+  delete[] vertexDataArr;
+  delete[] edgeDataArr;
+}
