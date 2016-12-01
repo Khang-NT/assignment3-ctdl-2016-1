f3f6b27bffad9846cb07b38876bded3623db7e69
Khang Nguyen <khang.neon.1997@gmail.com>
2016-12-01 14:31:15 +0700

    Refactor code: e8

--- main/tien_section.cpp
+++ main/tien_section.cpp
@@ -1,135 +1,102 @@
 #include "skeleton.h"
 
 struct StackItem {
-    Node      *Nodedata;
-    int        data;
+    Node      *data;
     StackItem *next;
+    StackItem(Node *data = NULL, StackItem *next = NULL) {
+        this->data = data;
+        this->next = next;
+    }
 };
 
 struct Stack
 {
     StackItem *top;
     int        count;
+    Stack() {
+        top   = 0;
+        count = 0;
+    }
+
+    bool isEmpty() {
+        return top == NULL;
+    }
+
+    void push(Node *data) {
+        top = new StackItem(data, top);
+        count++;
+    }
+
+    Node* pop() {
+        if (!isEmpty()) {
+            StackItem *item = top;
+            Node *result    = item->data;
+            top = top->next;
+            count--;
+            delete item;
+            return result;
+        }
+        return NULL;
+    }
 };
 
-bool isEmpty(Stack *s) {
-    if (!s) return 1;
-
-    if ((s->top == NULL) || (s->count == 0)) return 1;
-    else return 0;
-}
-
-void pushStack(int& d, Stack *& s, Node *& nodeData) {
-    if (!s) return;
-
-    StackItem *pNew = new StackItem;
-    pNew->data     = d;
-    pNew->Nodedata = nodeData;
-    pNew->next     = NULL;
-
-    if (isEmpty(s)) {
-        s->top   = pNew;
-        s->count = s->count + 1;
+int seach(Heap heap, int& tcout, int data, int count) {
+    for (int i = 0; i < count; i++) {
+        if (heap[i] == data) {
+            tcout = i;
+            return tcout;
+        }
     }
-    else {
-        pNew->next = s->top;
-        s->top     = pNew;
-        s->count   = s->count + 1;
-    }
-}
-
-void popStack(Stack *& s, int& data, Node *& nodeData) {
-    if (!(isEmpty(s))) {
-        StackItem *pDel = s->top;
-        data     = pDel->data;
-        nodeData = pDel->Nodedata;
-
-        s->top = s->top->next;
-        s->count--;
-
-        delete pDel, pDel = NULL;
-    }
+    return -1;
 }
 
 void e7() {
-    AVLTree avl7 = AVLTree();
-    int    *a;
-    int     count;
+    AVLTree *avl = new AVLTree();
+    int     *arr;
+    int      count;
 
-    ReadArrayInput("input/E7.txt", a, count);
+    ReadArrayInput("input/E7.txt", arr, count);
+    avl->ArrayToAVL(arr, count);
 
-    for (int i = 0; i < count; i++) {
-        avl7.AVLInsert(a[i]);
-    }
-    avl7.PrintAVL();
-    Node *root7 = avl7.root;
+    avl->PrintAVL();
 
-    StackItem *head = new StackItem;
-    head->data     = root7->data;
-    head->next     = NULL;
-    head->Nodedata = root7;
+    Stack *stack1 = new Stack();
+    stack1->push(avl->root);
 
-    Stack *stack1 = new Stack;
-    stack1->count = 1;
-    stack1->top   = head;
-    Stack *stack2 = new Stack;
-    stack2->count = 0;
-    stack2->top   = NULL;
-    int tong = 0;
-    int data = 0;
+    Stack *stack2 = new Stack();
 
-    while (stack1->top != NULL || stack2->top != NULL) {
-        data = 0;
-        Node *nodeData = NULL;
+    int   tong     = 0;
+    Node *nodeData = NULL;
 
-        if (stack1->top != NULL) {
-            while (stack1->top != NULL) {
-                if (stack1->top != NULL) {
-                    popStack(stack1, data, nodeData);
-                    tong = tong + data;
+    while (!stack1->isEmpty() || !stack2->isEmpty()) {
+        if (!stack1->isEmpty()) {
+            while (!stack1->isEmpty()) {
+                nodeData = stack1->pop();
+                tong     = tong + nodeData->data;
 
-                    if (nodeData->left != NULL) {
-                        pushStack(nodeData->left->data, stack2, nodeData->left);
+                if (nodeData->left) stack2->push(nodeData->left);
 
-                        // cout << "Kqstack2->top" << stack2->top->data;
-                    }
-
-                    if (nodeData->right != NULL) {
-                        pushStack(nodeData->right->data, stack2,
-                                  nodeData->right);
-
-                        // cout << "Kqstack22->top" << stack2->top->data;
-                    }
-                }
+                if (nodeData->right) stack2->push(nodeData->right);
             }
+            cout << tong << endl;
+            tong = 0;
         }
-        else {
-            if (stack2->top != NULL) {
-                while (stack2->top != NULL) {
-                    if (stack2->top != NULL) {
-                        popStack(stack2, data, nodeData);
-                        tong = tong + data;
 
-                        if (nodeData->left != NULL) {
-                            pushStack(nodeData->left->data,
-                                      stack1,
-                                      nodeData->left);
+        while (!stack2->isEmpty()) {
+            nodeData = stack2->pop();
+            tong     = tong + nodeData->data;
 
-                            // cout << "Kqstack1->top" << stack1->top->data;
-                        }
+            if (nodeData->left) stack1->push(nodeData->left);
 
-                        if (nodeData->right != NULL) {
-                            pushStack(nodeData->right->data,
-                                      stack1,
-                                      nodeData->right);
-
-                            // cout << "Kqstack12->top" << stack1->top->data;
-                        }
-                    }
-                }
-            }
+            if (nodeData->right) stack1->push(nodeData->right);
         }
         cout << tong << endl;
         tong = 0;
     }
+
+    delete avl;
+    delete stack1;
+    delete stack2;
 }
+
+void e8() {}
